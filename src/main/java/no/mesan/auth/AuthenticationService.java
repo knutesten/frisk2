e0cae2de316529599d98639c655a2cf376603d15
Knut Nekså <knutn@mesan.no>
2016-07-22 18:56:45 +0200

    Moved logic to service

--- src/main/java/no/mesan/auth/AuthenticationService.java
+++ src/main/java/no/mesan/auth/AuthenticationService.java
@@ -1,7 +1,48 @@
 package no.mesan.auth;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.jsonwebtoken.Jwts;
+import no.mesan.model.User;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
 /**
  * Created by knutn on 7/22/2016.
  */
 public class AuthenticationService {
+    private DiscoveryDocument discoveryDocument = new DiscoveryDocument();
+
+    public URI createLoginUrl() {
+        return UriBuilder.fromUri(discoveryDocument.getAuthorizationEndpoint())
+                .queryParam("client_id", discoveryDocument.getClientId())
+                .queryParam("response_type", "code")
+                .queryParam("scope", "openid email")
+                .queryParam("redirect_uri", "http://localhost:8080/auth/code").build();
+
+    }
+
+    public User exchangeCodeForAccessToken(String code) {
+        // TODO: Check that state equals that of the request.
+
+        Form form = new Form()
+                .param("code", code)
+                .param("client_id", discoveryDocument.getClientId())
+                .param("client_secret", discoveryDocument.getClientSecret())
+                .param("grant_type", "authorization_code")
+                .param("redirect_uri", "http://localhost:8080/auth/code");
+
+        Response response = ClientBuilder.newClient().target(discoveryDocument.getTokenEndpoint())
+                .request()
+                .post(Entity.form(form));
+
+        String idToken = response.readEntity(JsonNode.class).get("id_token").asText();
+        Jwts.parser().setSigningKeyResolver(new FriskSigningKeyResolver()).parseClaimsJws(idToken).getBody().get("email", String.class);
+
+        return null;
+    }
 }
--- src/main/java/no/mesan/resource/LoginResource.java
+++ src/main/java/no/mesan/resource/LoginResource.java
@@ -1,6 +1,7 @@
 package no.mesan.resource;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.jsonwebtoken.Jwts;
+import no.mesan.auth.AuthenticationService;
 import no.mesan.auth.DiscoveryDocument;
 import no.mesan.auth.FriskSigningKeyResolver;
 
@@ -19,39 +20,16 @@ import java.net.URI;
  */
 @Path("/auth")
 public class LoginResource {
-    private DiscoveryDocument discoveryDocument = new DiscoveryDocument();
-
+    private AuthenticationService authenticationService = new AuthenticationService();
     @GET
     @Path("/login")
     public Response redirectToConnectIdService() {
-        URI loginUrl = UriBuilder.fromUri(discoveryDocument.getAuthorizationEndpoint())
-                .queryParam("client_id", discoveryDocument.getClientId())
-                .queryParam("response_type", "code")
-                .queryParam("scope", "openid email")
-                .queryParam("redirect_uri", "http://localhost:8080/auth/code").build();
-
-        return Response.temporaryRedirect(loginUrl).build();
+        return Response.temporaryRedirect(authenticationService.createLoginUrl()).build();
     }
 
     @GET
     @Path("/code")
     public Response handleCode(@QueryParam("code") String code) {
-        // TODO: Check that state equals that of the request.
-
-        Form form = new Form()
-                .param("code", code)
-                .param("client_id", discoveryDocument.getClientId())
-                .param("client_secret", discoveryDocument.getClientSecret())
-                .param("grant_type", "authorization_code")
-                .param("redirect_uri", "http://localhost:8080/auth/code");
-
-        Response response = ClientBuilder.newClient().target(discoveryDocument.getTokenEndpoint())
-                .request()
-                .post(Entity.form(form));
-
-        String idToken = response.readEntity(JsonNode.class).get("id_token").asText();
-        Jwts.parser().setSigningKeyResolver(new FriskSigningKeyResolver()).parseClaimsJws(idToken).getBody().get("email", String.class);
-
-        return Response.temporaryRedirect(URI.create("/test")).build();
+        return Response.ok(authenticationService.exchangeCodeForAccessToken(code)).build();
     }
 }
